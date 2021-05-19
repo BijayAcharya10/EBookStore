7cac23e893948606e07cdbc2363ad6f4509c65c0
BijayAcharya10 <bijaysharma2057@gmail.com>
2021-05-19 22:08:34 +0545

    Book Covertype CRUD done

--- EBookStore.DataAccess/Migrations/20210519155736_addCoverTypeToDb.Designer.cs
+++ EBookStore.DataAccess/Migrations/20210519155736_addCoverTypeToDb.Designer.cs
@@ -4,14 +4,16 @@ using EBookStore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EBookStore.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210519155736_addCoverTypeToDb")]
+    partial class addCoverTypeToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EBookStore.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EBookStore.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType coverType);
+
+    }
+}
