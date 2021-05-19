c6d9cf5868519bd5c92aea45b0ffc3491ad86237
BijayAcharya10 <bijaysharma2057@gmail.com>
2021-05-19 21:23:21 +0545

    Book Category CRUD done

--- EBookStore.DataAccess/Repository/CategoryRepository.cs
+++ EBookStore.DataAccess/Repository/CategoryRepository.cs
@@ -23,7 +23,6 @@ namespace EBookStore.DataAccess.Repository
             if(objFromDb != null)
             {
                 objFromDb.Name = category.Name;
-                _db.SaveChanges();
             }
 
         }
--- EBookStore.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ EBookStore.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,5 +9,7 @@ namespace EBookStore.DataAccess.Repository.IRepository
         ICategoryRepository Category { get; }
 
         ISP_Call SP_Call { get; }
+
+        void Save();
     }
 }
